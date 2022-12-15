b31abf6fe33e5a8cc7e8834ae7db0b2c66785030
Karen Huaulme <karen.huaulme@gmail.com>
2022-12-14 23:11:41 -0600

    proper tailwind colors and aspect ratio corrected on tiktok url

--- src/components/TikTok.js
+++ src/components/TikTok.js
@@ -3,8 +3,8 @@ import React from "react";
 const TikTok = ({ author, description, TikTokURL }) => {
   console.log(TikTokURL);
   return (
-    <div className="flex flex-col w-96 rounded h-full text-white py-8 mx-auto bg-black px-2 text-sm content-start border border-yellow-200">
-      <div class="aspect-w-16 aspect-h-9">
+    <div className="flex flex-col w-96 rounded h-auto text-white py-8 mx-auto bg-black px-2 text-sm content-start border border-yellow-200">
+      <div class="aspect-w-9 aspect-h-16">
         <iframe
           title="tiktok"
           src={TikTokURL}
